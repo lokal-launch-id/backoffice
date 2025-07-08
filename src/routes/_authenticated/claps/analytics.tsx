22f4dd4744b39caeb22fff6f66a0eb2981507108
paulinavita <paulinadavita@gmail.com>
2025-07-08 11:11:29 +0100

    feat: remove unused clap analytics for now

--- src/routes/_authenticated/claps/analytics.tsx
+++ src/routes/_authenticated/claps/analytics.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import ComingSoon from '@/components/coming-soon'
 
 export const Route = createFileRoute('/_authenticated/claps/analytics')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_authenticated/claps/analytics"!</div>
+  return <ComingSoon />
 }
